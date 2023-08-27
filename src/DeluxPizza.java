906029a1e938ee4a1089cd284753b8343ca6cdac
Ajay Kumar Yadav <91870279+ajjushadow@users.noreply.github.com>
2023-08-27 11:10:15 +0530

    Add files via upload

---
+++ src/DeluxPizza.java
@@ -0,0 +1,19 @@
+public class DeluxPizza extends Pizza{
+    public DeluxPizza(Boolean veg) {
+        super(veg);
+        super.addExtraCheese();
+        super.addExtraToppings();
+    }
+
+    @Override
+    public void addExtraToppings() {}
+
+
+
+    @Override
+    public void addExtraCheese() {}
+
+
+    }
+
+
